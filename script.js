4ab7bba9f398103c3bfe8440f844d966169f8e51
SALAHEDDIN47 <salaheddinejalloul538@gmail.com>
2024-11-09 13:57:10 +0100

    working on the script

--- script.js
+++ script.js
@@ -100,3 +100,19 @@ products.forEach(
     `;
   }
 );
+
+class ShoppingCart {
+  constructor() {
+    this.items = [];
+    this.total = 0;
+    this.taxRate = 8.25;
+  }
+
+  addItem(id, products) {
+    const product = products.find((item) => item.id === id);
+    const { name, price } = product;
+    this.items.push(product);
+
+
+  }
+};
