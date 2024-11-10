6de4648910606547efa6ad7a7ee6c1a389bb91dc
SALAHEDDIN47 <salaheddinejalloul538@gmail.com>
2024-11-10 23:49:11 +0100

    working on the script

--- script.js
+++ script.js
@@ -113,6 +113,12 @@ class ShoppingCart {
     const { name, price } = product;
     this.items.push(product);
 
+    const totalCountPerProduct = {};
+    this.items.forEach((dessert) => {
+      totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
+    })
+
+    const currentProductCount = totalCountPerProduct[product.id];
 
   }
 };
