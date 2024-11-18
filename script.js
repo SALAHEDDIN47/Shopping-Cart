234e8f0acb8385112c8205ddc07c0de208f9a5f6
SALAHEDDIN47 <salaheddinejalloul538@gmail.com>
2024-11-18 17:31:07 +0100

    working on the script

--- script.js
+++ script.js
@@ -146,11 +146,13 @@ class ShoppingCart {
     const isCartCleared = confirm(
       "Are you sure you want to clear all items from your shopping cart?"
     );
-    if(isCartCleared){
+
+    if (isCartCleared) {
       this.items = [];
       this.total = 0;
+      productsContainer.innerHTML = "";
+      
     }
-
   }
 
   calculateTaxes(amount) {
