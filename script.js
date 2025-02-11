4431aabac3e6ca5f2aad10f029632aae60eb0909
SALAHEDDIN47 <salaheddinejalloul538@gmail.com>
2025-02-11 09:32:36 +0100

    modifying code

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ class ShoppingCart {
     this.items = [];
     this.total = 0;
     this.taxRate = 8.25;
-  }
+  }//Declaring the constructor
 
   addItem(id, products) {
     const product = products.find((item) => item.id === id);
